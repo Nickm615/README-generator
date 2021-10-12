ba3f1523b0a00482fc75ec1cefdb240e1a64e0d3
Nick Margaritondo <86981780+Nickm615@users.noreply.github.com>
2021-10-12 18:09:24 -0500

    Created an array of questions to be referenced by prompt

--- Develop/index.js
+++ Develop/index.js
@@ -2,7 +2,42 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title'
+    },
+    {
+        type: 'input',
+        message: 'Provide a short description of your project',
+        name: 'description'
+    },
+    {
+        type: 'input',
+        message: 'What steps are required to install your project? Please be thorough.',
+        name: 'installation'
+    },
+    {
+        type: 'input',
+        message: 'How is your project used? Please provide step-by-step instructions.',
+        name: 'instructions'
+    },
+    {
+        type:'input',
+        message:'Who did you collaborate with on this project? Please provide links their Github profiles',
+        name:'credits'
+    },
+    {
+        type:'list',
+        message:'What license would you like your project to be used under?',
+        choices:['MIT','Apache 2.0','BSD-3-Clause','BSD-2-Clause','GPL','LGPL','MPL-2.0','CDDL-1.0','EPL-2.0'],
+        name:'license'
+    }
+
+
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
